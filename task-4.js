d9c7e8c070187f17b15d6fd1be62df89b494be3f
samarkov <konstantin.samarkov@gmail.com>
2022-08-15 19:00:33 +0300

    add comments

--- task-4.js
+++ task-4.js
@@ -4,6 +4,7 @@
 // Таких приборов должно быть как минимум два (например, настольная лампа и компьютер). 
 // Выбрав прибор, подумайте, какими свойствами он обладает.
 
+// класс "электроприборы"
 function DomesticAppliance(name) {
     this.voltage = 220;
     this.name = name;
@@ -26,6 +27,7 @@ DomesticAppliance.prototype.checkPlugState = function () {
     return this.plugged;
 }
 
+// метод подсчета мощности
 DomesticAppliance.prototype.getWatt = function (amperage) {
     console.log(`${this.name}'s consuming power is ${this.voltage * amperage} watt`);
 }
@@ -34,43 +36,48 @@ DomesticAppliance.prototype.turnOn = function() {
     console.log(`${this.name} is turned on`);
 }
 
+//класс ТВ
 function Tv(name, place) {
     this.place = place;
     this.name = name;
     // override метода
     this.turnOn = function () {
-        console.log('TV is turned on, the screen is blue');
+        console.log(`TV ${this.name} is turned on, the screen is blue`);
     }
 }
 
-// Класс ТВ наследник класса бытовой прибор
+// Класс ТВ это наследник класса бытовой прибор, связь через свойсво prototype
 Tv.prototype = new DomesticAppliance();
 // метод для всех наследников TV
 Tv.prototype.switchProgram = function () {
     console.log('Channel is switched');
 }
 
-//управляем чайником
+// создаем экземпляр класса электроприборы - чайник
 const kettle = new DomesticAppliance('Kettle'); //чайник с потреблением 10А
-kettle.getWatt(10);
-kettle.unPlug();
+
+kettle.getWatt(10); 
+kettle.unPlug(); 
 kettle.turnOn();
 
-//управляем посудомойкой
+// создаем экземпляр класса электроприборы - поусдомойка
 const dishwasher = new DomesticAppliance('Dishwasher'); //посудомойка с потреблением 6А
+
 dishwasher.getWatt(6);
 dishwasher.plugIn();
 dishwasher.turnOn();
 
-//TV в спальне
+//создаем 1 экземпляр класса телевизоры
 const bedRoomTv = new Tv('Bedroom\'s TV', 'bedroom'); 
+
 bedRoomTv.plugIn();
 bedRoomTv.getWatt(1);
 bedRoomTv.switchProgram();//метод только у класса ТВ
-bedRoomTv.turnOn();
+bedRoomTv.turnOn(); // вместо метода класса электроприборы используется метод класса ТВ
 
-//TV в гостинной
+//создаем 2-ой экземпляр класса телевизоры
 const livingRoomTv = new Tv('Living room\'s TV', 'living room');  
+
 livingRoomTv.plugIn();
 livingRoomTv.getWatt(2);
 livingRoomTv.switchProgram();//метод только у класса ТВ
